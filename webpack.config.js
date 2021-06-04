513e45391e0defd21d6fa359f1aad4fe7f796707
Gabriel Lopes <gblsilvalopes@gmail.com>
2021-06-04 17:29:10 -0300

    ✨ Cria o primeiro componente em React JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,22 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-    entry: path
-}
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
