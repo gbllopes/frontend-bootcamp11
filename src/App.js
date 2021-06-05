4a6d0cad6d83f65330368355c192f7abcb381beb
Gabriel Lopes <gblsilvalopes@gmail.com>
2021-06-04 21:15:03 -0300

    ✨ Adiciona Componente header ao projeto.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header name="Gabriel" />
+      <h1>Hello GoStack</h1>
+    </>
+  );
 }
 
 export default App;
